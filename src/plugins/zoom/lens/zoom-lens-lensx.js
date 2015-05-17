3e2a4a61e8266e6029b9f8dd45d4edffe15e5cba
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-17 20:09:39 +0200

    introduce sample object to shift and manage animation for lens x and lens y

--- src/plugins/zoom/lens/zoom-lens-lensx.js
+++ src/plugins/zoom/lens/zoom-lens-lensx.js
@@ -14,18 +14,10 @@
 			config.barOrientation = 'Horizontal';
 			JenScript.AbstractPlusMinusBarWidget.call(this,config);
 			
-			var percents = ['0%','20%','50%','80%','100%'];
-		    var colors = [ 'rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0,0.6)', 'rgba(0, 0, 0, 0.8)', 'rgba(0, 0, 0, 0.6)','rgba(0, 0, 0, 0.1)' ];
-		    var buttonDrawColor = 'rgb(91,151,168)';
-		    var buttonRolloverDrawColor = 'rgb(247,239,100)';
-			
-		    //this.setShader({percents:percents, colors:colors});
 		    this.setOutlineStrokeColor((config.outlineStrokeColor !== undefined)?config.outlineStrokeColor : 'black');
 		    this.setOutlineFillColor(config.outlineFillColor);
 		    this.setButtonDrawColor((config.buttonStrokeColor !== undefined)?config.buttonStrokeColor : 'black');
-		    
 		    this.setButtonRolloverDrawColor((config.buttonRolloverStrokeColor !== undefined)?config.buttonRolloverStrokeColor : 'green');
-		    
 		    this.setOrphanLock(true);
 		},
 		
