cf366e7587a70fb7d34ecfc0ca67808a1880258c
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-26 23:52:00 +0200

    update header

--- src/header.js
+++ src/header.js
@@ -1,15 +1,9 @@
-// JenScript -  JavaScript SVG Framework
-// Product of JenSoftAPI - Visualization Frameworks
+// JenScript -  JavaScript HTML5/SVG Library
+// Product of JenSoftAPI - Visualization Java & JS Libraries
 // version : @VERSION
-//
-// Author : JenSoft API 
 // Author : Sebastien Janaud 
-// 
-// Web Site : http://www.jensoftapi.com/
+// Web Site : http://jenscript.io
 // Twitter  : http://twitter.com/JenSoftAPI
-// 
-//
 // Copyright (C) 2008 - 2015 JenScript, product by JenSoftAPI company, France.
 // build: @DATE
-// 
 // All Rights reserved
