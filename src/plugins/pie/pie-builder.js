7e8ad11aabd79e91cccf74b7a744f67096d570fc
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-31 21:46:50 +0200

    update pie builder

--- src/plugins/pie/pie-builder.js
+++ src/plugins/pie/pie-builder.js
@@ -10,11 +10,18 @@
 		pp.addPie(pie);
 		var fill = new JenScript.PieDefaultFill();
 		pie.setFill(fill);
+		
+		var labels = [];
+		var slices = [];
+		var effects = [];
 		var lastSlice;
+		
+		//improve with index 
 		var slice = function(config){
 			var s = new JenScript.PieSlice(config);
 			lastSlice = s;
 			pie.addSlice(s);
+			slices.push(s);
 			return this;
 		}
 		var label = function(type,config){
@@ -24,6 +31,7 @@
 			if('border' === type)
 				l = new JenScript.PieBorderLabel(config);
 			lastSlice.setSliceLabel(l);
+			labels.push(l);
 			return this;
 		}
 		var effect = function(type, config){
@@ -33,6 +41,7 @@
 			if('reflection' === type)
 				fx = new JenScript.PieReflectionEffect(config);
 			pie.addEffect(fx);
+			effects.push(fx);
 			return this;
 		}
 		var linearFx = function(config){
@@ -43,14 +52,21 @@
 			effect('reflection',config);
 			return this;
 		}
+		
+		//Pie Builder Interface
 		return {
 			slice : slice,
 			label : label,
 			effect : effect,
 			linearFx : linearFx,
 			reflectFx : reflectFx,
+			
 			view : function(){return view;},
 			projection : function(){return projection;},
+			pie : function(){return pie;},
+			labels : function(){return labels;},
+			slices : function(){return slice;},
+			effects : function(){return effects;},
 		};
 	};
 })();
