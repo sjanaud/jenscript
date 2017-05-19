724dc779712ec5331dff90174cc831be3d250aba
Sebastien Janaud <sebastien.janaud@gmail.com>
2017-05-19 16:54:42 +0200

    clean task

--- tasks/export.js
+++ tasks/export.js
@@ -3,13 +3,13 @@
 module.exports = function(grunt) {
 	grunt.registerTask('export', 'export jenscript to directory, usage $ grunt export:/path/to/dir', function(exportDir) {
 		  grunt.log.writeln('jenscript@export'+grunt.config('pkg').version);
-		  if(exportDir !== undefined){
+		  var fs = require('fs');
+		  if(exportDir !== undefined && fs.existsSync(exportDir)){
 			  grunt.log.writeln('jenscript@'+grunt.config('pkg').version+' export --> '+exportDir);
 			  grunt.config('exportDir',exportDir);
 			  grunt.task.run(["copy:export"]);
 		  }else{
-			  //grunt.log.writeln('jenscript@export not export directory supplied');
-			  grunt.fail.fatal("jenscript export directory should be supplied.")
+			  grunt.fail.fatal("jenscript valid export directory should be supplied.")
 		  }
 	});
 };
